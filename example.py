8dae75446e831753818b190e11938956b524a1b0
Mark Nutter <mnutter@amd.com>
2025-11-03 15:32:07 +0000

    example.py outputs latents not video [default]

--- example.py
+++ example.py
@@ -3,13 +3,33 @@ from fastvideo import VideoGenerator
 from fastvideo.fastvideo_args import FastVideoArgs
 
 def main():
-    os.environ["FASTVIDEO_ATTENTION_BACKEND"] = "VIDEO_SPARSE_ATTN"
+
+    if "FASTVIDEO_ATTENTION_BACKEND" not in os.environ:
+        os.environ["FASTVIDEO_ATTENTION_BACKEND"] = "VIDEO_SPARSE_ATTN"
+
+    SAVE_VIDEO=False
+    OUTPUT_PATH="my_videos/"
+
+    # resolution=(256,256)        # tiny
+    resolution=(640,480)        # 480p
+    # resolution=(1280,720)       # 720p
+    # resolution=(1920,1080)      # 1080p
+    WIDTH=resolution[0]
+    HEIGHT=resolution[1]
+    # NUM_FRAMES=125                # default
+    NUM_FRAMES=250
+
+    print('')
+    print('FASTVIDEO_ATTENTION_BACKEND:', os.environ["FASTVIDEO_ATTENTION_BACKEND"])
+    print('SAVE_VIDEO:', SAVE_VIDEO)
+    print('resolution:', resolution)
 
     # Create a video generator with a pre-trained model
     generator = VideoGenerator.from_pretrained(
         "FastVideo/FastWan2.1-T2V-1.3B-Diffusers",
         num_gpus=1,  # Adjust based on your hardware
         use_fsdp_inference=False,  # Disable FSDP inference
+        VSA_sparsity=0.90,
     )
 
     # Define a prompt for your video
@@ -18,10 +38,28 @@ def main():
     # Generate the video
     video = generator.generate_video(
         prompt,
-        return_frames=True,  # Also return frames from this call (defaults to False)
-        output_path="my_videos/",  # Controls where videos are saved
-        height=256, width=256,
-        save_video=True
+        output_path=OUTPUT_PATH,  # Controls where videos are saved
+        height=HEIGHT, width=WIDTH,
+        num_frames=NUM_FRAMES,
+        save_video=SAVE_VIDEO,
+        output_type="latent"
+    )
+
+    # Generate another video with a different prompt, without reloading the model!
+    prompt2 = (
+        "A majestic lion strides across the golden savanna, its powerful frame "
+        "glistening under the warm afternoon sun. The tall grass ripples gently in "
+        "the breeze, enhancing the lion's commanding presence. The tone is vibrant, "
+        "embodying the raw energy of the wild. Low angle, steady tracking shot, "
+        "cinematic.")
+
+    video2 = generator.generate_video(
+        prompt2, 
+        output_path=OUTPUT_PATH,
+        height=HEIGHT, width=WIDTH,
+        num_frames=NUM_FRAMES,
+        save_video=SAVE_VIDEO,
+        output_type="latent"
     )
 
 if __name__ == '__main__':
